c28794f233f745b16e3387c57fd67557b1434ee1
isholah360 <isholah360@gmail.com>
2023-03-07 05:58:42 -0800

    port update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Homepage } from './pages'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
-      portfolio
-       
+      <>
+      <Homepage/>
+      </>
     </div>
   )
 }
