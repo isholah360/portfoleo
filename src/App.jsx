746210e969a312e9816ba5f0635b62830b7e325b
ishola360 <isholah360.com>
2024-02-29 06:46:04 -0800

    cv

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Homepage } from './pages'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <>
+      <Homepage/>
+      </>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export {default as Content} from './headcontent/content'
+export {default as Content} from './aboutcontent/content'
